0de2630cc914631fb2aaf0b136bfc95b56612145
chengivan2 <chengoleivan@gmail.com>
2022-11-15 13:33:21 +0300

    First commit for JS payment API with JS

---
+++ jsrapi.js
@@ -0,0 +1,22 @@
+paypal.Buttons({
+    createOrder: function(data, actions) {
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '0.01' //Change amount
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+  }).render('#paypal-button-container');
+  //This function displays payment buttons on your web page.
+
+//All code is handled in the back end so no need to configure anything in this file.
